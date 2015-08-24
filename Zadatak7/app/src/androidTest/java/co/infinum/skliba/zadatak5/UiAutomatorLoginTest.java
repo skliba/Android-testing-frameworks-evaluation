759da5eca0dc59b5c81dadaa186895fe5f9eac37
skliba <skliba@foi.hr>
2015-08-25 00:21:15 +0200

    tweak UiAutomator testa

--- Zadatak7/app/src/androidTest/java/co/infinum/skliba/zadatak5/UiAutomatorLoginTest.java
+++ Zadatak7/app/src/androidTest/java/co/infinum/skliba/zadatak5/UiAutomatorLoginTest.java
@@ -1,5 +1,7 @@
 package co.infinum.skliba.zadatak5;
 
+import android.os.RemoteException;
+import android.support.test.uiautomator.UiCollection;
 import android.support.test.uiautomator.UiDevice;
 import android.support.test.uiautomator.UiObject;
 import android.support.test.uiautomator.UiObjectNotFoundException;
@@ -78,7 +80,29 @@ public class UiAutomatorLoginTest extends InstrumentationTestCase {
             //test baciti error
 
             assertTrue("Package name wrong", recyclerViewList.exists());
-        } catch (UiObjectNotFoundException e) {
+
+            device.setOrientationLeft();
+
+            UiCollection recy = new UiCollection(new UiSelector()
+                    .className("android.support.v7.widget.RecyclerView"));
+
+            UiObject ispravljamZadace = recy.getChildByText(new UiSelector()
+                    .className("android.widget.RelativeLayout"), "Ispravljam zadace");
+            ispravljamZadace.click();
+
+            UiObject upboatButton = device.findObject(new UiSelector()
+                    .resourceId("co.infinum.skliba.zadatak5:id/button_upboat"));
+
+            upboatButton.click();
+
+            UiObject detailsImage = device.findObject(new UiSelector()
+                    .resourceId("co.infinum.skliba.zadatak5:id/details_picture"));
+
+            //ista stvar kao i iznad, samo ! ispred booleana pokazat ce jel se izvrti taj dio koda
+            
+            assertTrue("Image is not shown", detailsImage.exists());
+
+        } catch (UiObjectNotFoundException | RemoteException e) {
             e.printStackTrace();
         }
 
